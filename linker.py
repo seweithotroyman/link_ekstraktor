7927739794716a3de8df2a1e6ed33fd95d5a23f7
seweithotroyman <seweithotroyman@gmail.com>
2019-04-27 00:57:10 +0700

    Create linker.py

---
+++ linker.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 27 00:24:14 2019
+
+@author: hehehe
+"""
+
+from urllib.request import urlopen, urljoin
+import re
+
+def download_halaman(url):
+    return urlopen(url).read().decode('utf-8')
+
+def hasilkan_link(page):
+    cocokan_link = re.compile('<a[^>]+href=["\'](.*?)["\']',
+                            re.IGNORECASE)
+    return cocokan_link.findall(page)
+
+if __name__ == '__main__':
+    target_url = 'https://www.apress.com/'
+    website = download_halaman(target_url)
+    links = hasilkan_link(website)
+    
+    for link in links:
+        print(urljoin(target_url, link))
